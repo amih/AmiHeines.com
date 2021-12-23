b6931d13a45e6c880cade53e13b6414b61cf4398
Ami Heines <ami.heines@gmail.com>
2021-12-23 20:12:50 +0200

    try 11ty

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: 'Ami Heines',
+  description: 'EOSIO, META-II, SQL2EOS blog',
+  keywords: ['EOSIO', 'META-II', 'SQL2EOS', 'EOS'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://amiheines.com',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
